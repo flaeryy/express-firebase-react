4642d3b3ee091d369a144002c302ce1445de599b
Božidar Nađ <48714324+flaeryy@users.noreply.github.com>
2023-05-20 23:17:39 +0200

    modified for vercel

--- api/index.js
+++ api/index.js
@@ -4,16 +4,16 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "../../frontend/build/index.html"),
+    path.join(__dirname, "../frontend/build/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
