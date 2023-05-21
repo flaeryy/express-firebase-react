17dc2020ed72eb918833aa5a4e3febbe780baedb
Božidar Nađ <48714324+flaeryy@users.noreply.github.com>
2023-05-21 23:04:49 +0200

    adding in front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>test 123</p>
+        <p>test 123 hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
